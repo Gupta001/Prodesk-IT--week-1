63628d24f98d666243686a3a288a6fd24c952df6
Ketan Bhaskar <ketanbhaskar1@gmail.com>
2026-01-12 01:19:29 +0530

    Updated responsive design, dark mode and mobile navbar

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const toggleBtn = document.getElementById("themeToggle");
+  const themeBtn = document.getElementById("themeToggle");
+  const menuToggle = document.getElementById("menuToggle");
+  const navLinks = document.getElementById("navLinks");
 
-  toggleBtn.addEventListener("click", function () {
+  // Dark mode toggle
+  themeBtn.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
+    themeBtn.textContent = document.body.classList.contains("dark-mode")
+      ? "☀️"
+      : "🌙";
+  });
 
-    if (document.body.classList.contains("dark-mode")) {
-      toggleBtn.textContent = "☀️";
-    } else {
-      toggleBtn.textContent = "🌙";
-    }
+  // Mobile menu toggle
+  menuToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
   });
 });
